ca663de34b12423ba879f86f7e9a0c62a58333c8
George  Ikwegbu Chinedu <39649680+gikwegbu@users.noreply.github.com>
2023-02-22 10:41:15 +0100

    Updated the Report UI
    1. Added an Report Overview screen to display the overall processes.
    
    2. Added internalisation to help format datetime.
    
    3. Used willpopscope to prevent users from leaving the app.

--- lib/app/modules/home/widgets/add_dialog.dart
+++ lib/app/modules/home/widgets/add_dialog.dart
@@ -13,139 +13,142 @@ class AddDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        key: homeCtrl.formKey,
-        child: ListView(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 3.0.wp),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                    splashRadius: 20,
-                    onPressed: () {
-                      Get.back();
-                      homeCtrl.editCtrl.clear();
-                      homeCtrl.changeTask(null);
-                    },
-                    icon: const Icon(Icons.close),
-                  ),
-                  TextButton(
-                    style: ButtonStyle(
-                      overlayColor:
-                          MaterialStateProperty.all(Colors.transparent),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        body: Form(
+          key: homeCtrl.formKey,
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 3.0.wp),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      splashRadius: 20,
+                      onPressed: () {
+                        Get.back();
+                        homeCtrl.editCtrl.clear();
+                        homeCtrl.changeTask(null);
+                      },
+                      icon: const Icon(Icons.close),
                     ),
-                    onPressed: () {
-                      if(homeCtrl.formKey.currentState!.validate()) {
-                        if(homeCtrl.task.value == null) {
-                          EasyLoading.showError("Please select task type");
-                        }else  {
-                          var success = homeCtrl.updateTask(
-                            homeCtrl.task.value!,
-                            homeCtrl.editCtrl.text,
-                          );
-                          if(success) {
-                            EasyLoading.showSuccess("Todo Added 📝", maskType: EasyLoadingMaskType.black);
-                            Get.back();
-                            homeCtrl.changeTask(null);
-                          } else {
-                            EasyLoading.showError("Todo item already exist");
+                    TextButton(
+                      style: ButtonStyle(
+                        overlayColor:
+                            MaterialStateProperty.all(Colors.transparent),
+                      ),
+                      onPressed: () {
+                        if(homeCtrl.formKey.currentState!.validate()) {
+                          if(homeCtrl.task.value == null) {
+                            EasyLoading.showError("Please select task type");
+                          }else  {
+                            var success = homeCtrl.updateTask(
+                              homeCtrl.task.value!,
+                              homeCtrl.editCtrl.text,
+                            );
+                            if(success) {
+                              EasyLoading.showSuccess("Todo Added 📝", maskType: EasyLoadingMaskType.black);
+                              Get.back();
+                              homeCtrl.changeTask(null);
+                            } else {
+                              EasyLoading.showError("Todo item already exist");
+                            }
+                            homeCtrl.editCtrl.clear();
                           }
-                          homeCtrl.editCtrl.clear();
                         }
-                      }
-                    },
-                    child: labelText(
-                      'Done',
-                      fontSize: 14.0.sp,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
-              child: labelText(
-                "New Task",
-                fontWeight: FontWeight.bold,
-                fontSize: 20.0.sp,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
-              child: TextFormField(
-                controller: homeCtrl.editCtrl,
-                decoration: InputDecoration(
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.grey[400]!,
-                    ),
-                  ),
+                      },
+                      child: labelText(
+                        'Done',
+                        fontSize: 14.0.sp,
+                      ),
+                    )
+                  ],
                 ),
-                autofocus: true,
-                validator: (val) {
-                  if (val == null || val.trim().isEmpty) {
-                    return "Field cannot be empty";
-                  }
-                  return null;
-                },
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 5.0.wp,
-                left: 5.0.wp,
-                bottom: 5.0.wp,
-                right: 5.0.wp,
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+                child: labelText(
+                  "New Task",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0.sp,
+                ),
               ),
-              child: labelText(
-                "Add to",
-                fontSize: 14.0.sp,
-                color: Colors.grey,
-              ),
-            ),
-            ...homeCtrl.tasks
-                .map(
-                  (element) => Obx(() =>
-                    InkWell(
-                      onTap: () => homeCtrl.changeTask(element),
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 5.0.wp, vertical: 3.0.wp),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                Icon(
-                                  IconData(element.icon, fontFamily: 'MaterialIcons'),
-                                  color: HexColor.fromHex(element.color),
-                                ),
-                                SizedBox(
-                                  width: 3.0.wp,
-                                ),
-                                labelText(
-                                  element.title,
-                                  fontSize: 12.0.sp,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ],
-                            ),
-                            if (homeCtrl.task.value == element)
-                              const Icon(
-                                Icons.check,
-                                color: blue,
-                              )
-                          ],
-                        ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 5.0.wp),
+                child: TextFormField(
+                  controller: homeCtrl.editCtrl,
+                  decoration: InputDecoration(
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey[400]!,
                       ),
                     ),
                   ),
-                )
-                .toList()
-          ],
+                  autofocus: true,
+                  validator: (val) {
+                    if (val == null || val.trim().isEmpty) {
+                      return "Field cannot be empty";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 5.0.wp,
+                  left: 5.0.wp,
+                  bottom: 5.0.wp,
+                  right: 5.0.wp,
+                ),
+                child: labelText(
+                  "Add to",
+                  fontSize: 14.0.sp,
+                  color: Colors.grey,
+                ),
+              ),
+              ...homeCtrl.tasks
+                  .map(
+                    (element) => Obx(() =>
+                      InkWell(
+                        onTap: () => homeCtrl.changeTask(element),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 5.0.wp, vertical: 3.0.wp),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    IconData(element.icon, fontFamily: 'MaterialIcons'),
+                                    color: HexColor.fromHex(element.color),
+                                  ),
+                                  SizedBox(
+                                    width: 3.0.wp,
+                                  ),
+                                  labelText(
+                                    element.title,
+                                    fontSize: 12.0.sp,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ],
+                              ),
+                              if (homeCtrl.task.value == element)
+                                const Icon(
+                                  Icons.check,
+                                  color: blue,
+                                )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList()
+            ],
+          ),
         ),
       ),
     );
--- lib/app/modules/report/view.dart
+++ lib/app/modules/report/view.dart
@@ -1,12 +1,169 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx/app/core/utils/extensions.dart';
+import 'package:getx/app/core/utils/texts.dart';
+import 'package:getx/app/core/values/colors.dart';
+import 'package:getx/app/modules/home/controller.dart';
+import 'package:intl/intl.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 class ReportScreen extends StatelessWidget {
-  const ReportScreen({Key? key}) : super(key: key);
+  final homeCtrl = Get.find<HomeController>();
+
+  ReportScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Hello")
+      body: SafeArea(
+        child: Obx(
+          () {
+            var createdTasks = homeCtrl.getTotalTask();
+            var completedTasks = homeCtrl.getTotalDoneTasks();
+            var liveTasks = createdTasks - completedTasks;
+            var percentage =
+                (completedTasks / createdTasks * 100).toStringAsFixed(0);
+            return ListView(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(4.0.wp),
+                  child: labelText(
+                    "My Report",
+                    fontSize: 24.0.sp,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(4.0.wp),
+                  child: labelText(
+                    DateFormat.yMMMMd().format(DateTime.now()),
+                    fontSize: 14.0.sp,
+                    color: Colors.grey,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 3.0.wp,
+                    vertical: 4.0.wp,
+                  ),
+                  child: const Divider(
+                    thickness: 2,
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 5.0.wp,
+                    vertical: 3.0.wp,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildStatus(
+                        title: 'Live Tasks',
+                        number: liveTasks,
+                        color: Colors.green,
+                      ),
+                      _buildStatus(
+                        title: 'Completed',
+                        number: completedTasks,
+                        color: Colors.orange,
+                      ),
+                      _buildStatus(
+                        title: 'Created Tasks',
+                        number: createdTasks,
+                        color: Colors.blue,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0.wp,
+                ),
+                UnconstrainedBox(
+                  child: SizedBox(
+                    width: 70.0.wp,
+                    height: 70.0.wp,
+                    child: CircularStepProgressIndicator(
+                      totalSteps: createdTasks == 0 ? 1 : createdTasks,
+                      currentStep: completedTasks,
+                      stepSize: 20,
+                      selectedColor: green,
+                      unselectedColor: Colors.grey[200],
+                      padding: 0,
+                      width: 150,
+                      height: 150,
+                      selectedStepSize: 22,
+                      roundedCap: (_, __) => true,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          labelText(
+                            "${createdTasks == 0 ? 0 : percentage}%",
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                          SizedBox(height: 1.0.wp,),
+                          labelText(
+                            "Efficiency",
+                            color: Colors.grey,
+                            fontSize: 12.0.sp,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStatus({
+    required Color color,
+    required int number,
+    required String title,
+  }) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 3.0.wp,
+          width: 3.0.wp,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              width: 0.5.wp,
+              color: color,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 3.0.wp,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            labelText(
+              '$number',
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0.sp,
+            ),
+            SizedBox(
+              width: 3.0.wp,
+            ),
+            labelText(
+              title,
+              fontWeight: FontWeight.bold,
+              fontSize: 10.0.sp,
+              color: Colors.grey,
+            ),
+          ],
+        )
+      ],
     );
   }
 }
